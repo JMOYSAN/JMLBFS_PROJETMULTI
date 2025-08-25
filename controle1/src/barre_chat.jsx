5e5d850b87a3470a775213326eb14da6ed2c3eb7
6253494 <santerre.francois371@gmail.com>
2025-08-25 15:10:34 -0400

    Ajout ui de base et barre de chat

---
+++ controle1/src/barre_chat.jsx
@@ -0,0 +1,11 @@
+function Chat() {
+    return (
+        <div id="chat-bar">
+            <input type="text" id="chat-input" placeholder="Type a message..." />
+            <button id="send-btn">Send</button>
+        </div>
+    );
+}
+
+
+export default Chat;

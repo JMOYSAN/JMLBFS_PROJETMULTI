003f91c3de920731c850f7aaa90a9fe08a1bcda5
JMOYSAN <2375142@cstjean.qc.ca>
2025-08-28 12:43:30 -0400

    Fix generales pour le projet

--- controle1/src/App.jsx
+++ controle1/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Chat from './Messages/BarreChat';
+import Chat  from './Messages/BarreChat';
 import FilsConversation from './Messages/FilsConversation';
 
 function App() {

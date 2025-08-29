9816350617708ba5efc8acafc27a60882f940eb5
JMOYSAN <2375142@cstjean.qc.ca>
2025-08-29 12:57:07 -0400

    ajout fullscreen

--- controle1/electron/main.ts
+++ controle1/electron/main.ts
@@ -38,7 +38,7 @@ function createWindow() {
   })
   win?.webContents.openDevTools();
 
-
+  win.setFullScreen(true);
   // Test active push message to Renderer-process.
   win.webContents.on('did-finish-load', () => {
     win?.webContents.send('main-process-message', (new Date).toLocaleString())

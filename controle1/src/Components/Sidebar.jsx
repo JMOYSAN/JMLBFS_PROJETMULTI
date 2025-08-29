9fc7041cd0a91f1394125d3f0d4260835ff26770
JMOYSAN <2375142@cstjean.qc.ca>
2025-08-29 13:50:27 -0400

    Ajout details pour chaque msg + css de testing

--- controle1/src/Components/Sidebar.jsx
+++ controle1/src/Components/Sidebar.jsx
@@ -1,13 +1,7 @@
 function Sidebar({onLogout}) {
     return (
         <div className="sidebar">
-            <h2>Sidebar</h2>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
                 <button id="deconnexion" onClick={onLogout}>Déconnexion</button>
-            </ul>
         </div>
     );
 }

54e06c6c300655e4b8f275111d19559ad27ab429
JMOYSAN <2375142@cstjean.qc.ca>
2025-09-05 11:36:19 -0400

    Changement du default nom de l'appli

--- controle1/electron/main.ts
+++ controle1/electron/main.ts
@@ -28,7 +28,7 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
-  app.setAppUserModelId('com.example.myapp')
+  app.setAppUserModelId('Rogue Rats Chat')
   process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
     ? path.join(process.env.APP_ROOT!, 'public')
     : RENDERER_DIST
